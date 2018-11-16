383ca6c19bae95c782d124f188a13e2586be90f7
StealtyPanda <42720778+StealthyPanda@users.noreply.github.com>
2018-11-16 11:42:13 +0400

    finished the grapher

--- here.py
+++ here.py
@@ -1,13 +1,15 @@
 from board import mn_board as board
 
 #getting input
-r = int(raw_input('Enter rows: '))
-c = int(raw_input('Enter cols: '))
+"""r = int(raw_input('Enter rows: '))
+c = int(raw_input('Enter cols: '))"""
+r = 51
+c = 51
 origin = [r/2+1, c/2+1]
 
 
 #init stuff
-graph = board(r, c, 'O')
+graph = board(r, c, ' ')
 for each in range(c):
 	graph.put_at(r/2 + 1, each, '-')
 for each in range(r):
@@ -17,47 +19,80 @@ graph.put_at(r/2 + 1, c/2 + 1, '+')
 #getting the equation
 
 equation = raw_input('Enter the equation in x: \n')
-config = []
 
-for each in range(len(equation)):
+def getcon(equation):
+	config = []#main thing
+	const = 0
+	signs = []
 
-	sign = '+'
-	coeff = 0
-	power = 0
-		
+	if equation[0] not in ['+', '-']: equation = '+'+equation
+
+	for each in equation:
+		if each in ['+', '-']: signs.append(each)
+
+	equation = ' '.join(equation.split('+'))
+	equation = ' '.join(equation.split('-'))
+	equation = equation.split(' ')
 
 
-	if each != 0:
 
-		if not equation[each].isalpha() and equation[each] not in ['+', '-']:
-			try:
-				equation[each-1]
-			except IndexError:
-				pass
+	#clean up
+	for each in equation:
+		if not each: equation.remove(each)
+
+	for each in range(len(equation)):
+		if 'x' in equation[each]:
+			sign = signs[each]
+			coeff = equation[each].split('x')[0]
+			power = equation[each].split('x')[1][1:]
+			config.append([sign, coeff, power])
+		else:
+			const = int(signs[each] + equation[each])
+
+	return [config, const]
+
+
+print getcon(equation)
+
+
+def plot(equation, start = -10, end = 10):
+
+	equation = getcon(equation)
+	config = equation[0]
+	const = equation[1]
+	#y = const
+
+	pairs = []
+	#goin thru range
+	for x in range(start, end+1):
+		y = const
+		#goin thru each term
+		for each in config:
+			sign = 1
+			coeff=0
+			power=0
+			if sign == '-': sign = -1
+			if each[1]:
+				coeff = int(each[1])
 			else:
-				sign = equation[each-1]
-			coeff = int(equation[each])
-			try:
-				equation[each+1]
-			except IndexError:
+				coeff = 1
+
+			if each[2]:
+				power = int(each[2])
+			else:
 				power = 1
-			else:
-				if equation[each+1] == '^':
-					power = int(equation[each+2])
 
-		config.append([sign, coeff, power])
+			xhere = coeff*(x**power)*sign
+			y += xhere
+		pairs.append([x, y])
 
-	else:
+	for each in pairs:
+		graph.put_at(origin[1]-each[0], origin[0]+each[1], 'X')
 
-		config.append()
-
-
-print config
+	graph.print_board()
 
 
-
-
-
+plot(equation)
 
 
 
