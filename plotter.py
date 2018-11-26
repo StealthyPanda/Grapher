0dff0e36cc6562d2e979d2c9078d7870a689f3f4
StealtyPanda <42720778+StealthyPanda@users.noreply.github.com>
2018-11-26 18:19:21 +0400

    made a MAJOR update
    turns out im an idiot

--- plotter.py
+++ plotter.py
@@ -9,73 +9,17 @@ origin = [r/2+1, c/2+1]
 #getting the equation
 
 
-def getcon(equation):
-	config = []#main thing
-	const = 0
-	signs = []
-
-	if equation[0] not in ['+', '-']: equation = '+'+equation
-
-	for each in equation:
-		if each in ['+', '-']: signs.append(each)
-
-	equation = ' '.join(equation.split('+'))
-	equation = ' '.join(equation.split('-'))
-	equation = equation.split(' ')
-
-
-
-	#clean up
-	for each in equation:
-		if not each: equation.remove(each)
-
-	for each in range(len(equation)):
-		if 'x' in equation[each]:
-			sign = signs[each]
-			coeff = equation[each].split('x')[0]
-			power = equation[each].split('x')[1][1:]
-			config.append([sign, coeff, power])
-		else:
-			const = int(signs[each] + equation[each])
-
-	return [config, const]
-
-
-#print getcon(equation)
-
 
 def plot(graph, equation, start = -15, end = 15):
 
-	equation = getcon(equation)
-	config = equation[0]
-	const = equation[1]
-	#y = const
+	equation = equation.replace('^', '**').lower()
 
 	pairs = []
 	#goin thru range
 	for x in range(start, end+1):
-		y = 'no'
-		xhere = 0
-		#goin thru each term
-		for each in config:
-			sign = 1
-			coeff=0
-			power=0
-			if each[0] == '-': sign = -1
-			if each[1]:
-				coeff = float(each[1])
-			else:
-				coeff = 1
+		pairs.append([x, eval(equation.replace('x', str(x)))])
 
-			if each[2]:
-				power = float(each[2])
-			else:
-				power = 1
-			if not(x < 0 and isinstance(power, float)):
-				y = const
-				xhere = coeff*(x**power)*sign
-				y += xhere
-		pairs.append([x, y])
+
 	#print pairs
 	ytent = (r-1)/2
 	xtent = (c-1)/2
